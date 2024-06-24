25351077c8dd039980701561ea1510df09b606c6
rsmith-github <rsmith.git@gmail.com>
2024-06-24 18:23:12 +0100

    send random number if connection established

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const WebSocketServer = require("ws").Server;
 
-const wss = new WebSocketServer({port: 8080});
+const wss = new WebSocketServer({ port: 8080 });
 
 wss.on('connection', (ws) => {
 
     console.log("Incoming Connection")
 
 });
+
+
+setInterval(() => {
+
+    // 0-14 random number
+    const random_number = Math.random(Math.random() * 14)
+    wss.clients.forEach((ws) => {
+        if (ws.readyState === ws.OPEN) {
+            
+            ws.send(random_number)
+
+        }
+    })
+}, 10000)
 
