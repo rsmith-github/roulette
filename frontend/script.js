4731276b77470e02b8100ffa200d131808c4b409
rsmith-github <rsmith.git@gmail.com>
2024-06-28 20:47:55 +0100

    fix websocket secure connection issue when port forwarding

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const socket = new WebSocket('ws://localhost:8080');
+const socket = new WebSocket('wss://<tunnel-id>.uks1.devtunnels.ms/');
 
 document.addEventListener('DOMContentLoaded', function () {
 
